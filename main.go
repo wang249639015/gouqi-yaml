923f9d81c67419694804e37f98a988bae16dc0d0
wanglili <lili.wang18@pactera.com>
2020-07-18 14:14:22 +0800

    add read Yaml method

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package gouqi_yaml
+
+import "os"
+
+// 用于读取gouqi 工程下的gouqi.yaml 并可实现可插拔配置。
+// 实现模式
+func init() {
+
+}
+
+
+
+
+func ReadYaml(path string){
+
+}
+
+func ReadYamlByFile(file os.File){
+
+	
+}
